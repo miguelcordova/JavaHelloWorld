e833268c90da407511ff10b5b61c850840fbf3ee
miguelcordova <miguelcordovar@gmail.com>
2015-07-05 21:42:40 -0500

    Initial commit

---
+++ src/main/java/pe/com/mcordova/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package pe.com.mcordova.hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		LocalTime currentTime = new LocalTime();
+		
+		System.out.println("The current local time is: " + currentTime);
+	
+		System.out.println("Hello World Java");
+
+	}
+
+}
